2dd59d330adc84ad29eba4d9eb44bf79533170f3
Harpal <rajanbabrah056@gmail.com>
2023-01-04 01:16:45 +0530

    add error handlers

--- public/scripts/home-script.js
+++ public/scripts/home-script.js
@@ -11,59 +11,8 @@ const searchCity = document.querySelector("#search-city")
 const todayDate = document.querySelector(".current-date")
 const todayTime = document.querySelector(".current-time")
 const sunSetTime = document.querySelector(".sun-set")
-
-const topLocation1 = document.querySelector(".top-location-1")
-
-
-const divTemperature1 = document.querySelector(".temperature-1")
-const divHumidity1 = document.querySelector(".humidity-1")
-const divWindSpeed1 = document.querySelector(".wind-speed-1")
-const divPressure1 = document.querySelector(".pressure-1")
-const todayDate1 = document.querySelector(".current-day-1")
-const weatherIcon1 = document.querySelector(".weather-icon-1")
-const weatherDesc1 = document.querySelector(".weather-desc-1")
-
-const divTemperature2 = document.querySelector(".temperature-2")
-const divHumidity2 = document.querySelector(".humidity-2")
-const divWindSpeed2 = document.querySelector(".wind-speed-2")
-const divPressure2 = document.querySelector(".pressure-2")
-const todayDate2 = document.querySelector(".current-day-2")
-const weatherIcon2 = document.querySelector(".weather-icon-2")
-const weatherDesc2 = document.querySelector(".weather-desc-2")
-
-const divTemperature3 = document.querySelector(".temperature-3")
-const divHumidity3 = document.querySelector(".humidity-3")
-const divWindSpeed3 = document.querySelector(".wind-speed-3")
-const divPressure3 = document.querySelector(".pressure-3")
-const todayDate3 = document.querySelector(".current-day-3")
-const weatherIcon3 = document.querySelector(".weather-icon-3")
-const weatherDesc3 = document.querySelector(".weather-desc-3")
-
-const divTemperature4 = document.querySelector(".temperature-4")
-const divHumidity4 = document.querySelector(".humidity-4")
-const divWindSpeed4 = document.querySelector(".wind-speed-4")
-const divPressure4 = document.querySelector(".pressure-4")
-const todayDate4 = document.querySelector(".current-day-4")
-const weatherIcon4 = document.querySelector(".weather-icon-4")
-const weatherDesc4 = document.querySelector(".weather-desc-4")
-
-const divTemperature5 = document.querySelector(".temperature-5")
-const divHumidity5 = document.querySelector(".humidity-5")
-const divWindSpeed5 = document.querySelector(".wind-speed-5")
-const divPressure5 = document.querySelector(".pressure-5")
-const todayDate5 = document.querySelector(".current-day-5")
-const weatherIcon5 = document.querySelector(".weather-icon-5")
-const weatherDesc5 = document.querySelector(".weather-desc-5")
-
-const divTemperature6 = document.querySelector(".temperature-6")
-const divHumidity6 = document.querySelector(".humidity-6")
-const divWindSpeed6 = document.querySelector(".wind-speed-6")
-const divPressure6 = document.querySelector(".pressure-6")
-const todayDate6 = document.querySelector(".current-day-6")
-const weatherIcon6 = document.querySelector(".weather-icon-6")
-const weatherDesc6 = document.querySelector(".weather-desc-6")
-
-
+const ErrMsg = document.querySelector("#errMsg")
+const ErrMsgCity = document.querySelector("#errMsgCity")
 
 const weather = {};
 
@@ -74,7 +23,6 @@ weather.temperature = {
 const KELVIN = 273;
 
 const key = "fb0d9be033d9deb37814eb9956001d2c"
-const cnt = "7"
 
 if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
@@ -82,7 +30,14 @@ if ('geolocation' in navigator) {
     alert("Location not found")
 }
 function showError() {
-    alert("couldn't find Location")
+    ErrMsg.innerHTML = `<div class="alert alert-warning d-flex align-items-center" role="alert">
+    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:">
+        <use xlink:href="#exclamation-triangle-fill" />
+    </svg>
+    <div>
+    couldn't find Your location! Please turn on Your location
+    </div>
+</div>`
 }
 
 
@@ -91,7 +46,7 @@ async function getCity(city) {
     await fetch(api)
         .then(async function (response) {
             let data = await response.json();
-            console.log("newapi",api)
+            console.log("newapi", api)
             return data;
         })
         .then(async function (data) {
@@ -108,7 +63,7 @@ searchBtn.addEventListener('click', (event) => {
 
 
 function setPosition(position) {
-    // console.log(position)
+    console.log(position)
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     getUserLocation(latitude, longitude);
@@ -145,22 +100,26 @@ async function getUserLocation(latitude, longitude) {
 
 async function getWeather(data) {
     // console.log("city is", data);
-    let api = `https://api.openweathermap.org/data/2.5/forecast?q=${data.city.name}&appid=${key}`;
-    await fetch(api)
-        .then(async function (response) {
-            let data = await response.json();
-            return data;
-        })
-        .then(async function (data) {
-            displayWeather(data)
-        })
+    try {
+        let api = `https://api.openweathermap.org/data/2.5/forecast?q=${data.city.name}&appid=${key}`;
+        await fetch(api)
+            .then(async function (response) {
+                let data = await response.json();
+                return data;
+            })
+            .then(async function (data) {
+                displayWeather(data)
+            })
+    } catch (e) {
+        alert("Please Enter a Valid Location?")
+    }
 }
 
 
 
 
 function displayWeather(data) {
-    console.log("new",data)
+    // console.log("new",data)
     let secs = data.list[0].dt;
     // console.log(secs);
     const output = new Date(secs * 1000);
@@ -185,15 +144,15 @@ function displayWeather(data) {
     let s = new Date(sunSet * 1000);
     theDate = new Date(Date.parse(s));
     let sunsetTime = theDate.toLocaleTimeString();
-    console.log(sunSetTime);
+    // console.log(sunSetTime);
 
 
 
 
-    let temperature0 = Math.floor(data.list[0].main.temp - KELVIN);
+    let temperature = Math.floor(data.list[0].main.temp - KELVIN);
     topLocation.innerHTML = `${data.city.name}, ${data.city.country}`;
     divLocation.innerHTML = `${data.city.name}, ${data.city.country}`;
-    divTemperature.innerHTML = `${temperature0} <span>&#8451;</span>`;
+    divTemperature.innerHTML = `${temperature} <span>&#8451;</span>`;
     weatherDesc.innerHTML = data.list[0].weather[0].description;
     weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png">`;
     divHumidity.innerHTML = `${data.list[0].main.humidity} %`;
@@ -203,66 +162,6 @@ function displayWeather(data) {
     todayTime.innerHTML = CurrentTime;
     sunSetTime.innerHTML = sunsetTime;
 
-
-    topLocation1.innerHTML = `${data.city.name}, ${data.city.country}`;
-
-    let temperature1 = Math.floor(data.list[2].main.temp - KELVIN);
-    divTemperature1.innerHTML = `${temperature1} <span>&#8451;</span>`;
-    weatherDesc1.innerHTML = data.list[2].weather[1].description;
-    weatherIcon1.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[2].weather[0].icon}@2x.png">`;
-    divHumidity1.innerHTML = `${data.list[2].main.humidity} %`;
-    divWindSpeed1.innerHTML = `${data.list[2].wind.speed} km/h  ` + result;
-    divPressure1.innerHTML = `${data.list[2].main.pressure} mBar`;
-    todayDate1.innerHTML = finalR;
-    // topLocation1.innerHTML = `${data.city.name}, ${data.city.country}`;
-
-    console.log("111",divTemperature1);
-
-
-
-    let temperature2 = Math.floor(data.list[2].main.temp - KELVIN);
-    divTemperature2.innerHTML = `${temperature2} <span>&#8451;</span>`;
-    weatherDesc2.innerHTML = data.list[2].weather[0].description;
-    weatherIcon2.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[2].weather[0].icon}@2x.png">`;
-    divHumidity2.innerHTML = `${data.list[2].main.humidity} %`;
-    divWindSpeed2.innerHTML = `${data.list[2].wind.speed} km/h  ` + result;
-    divPressure2.innerHTML = `${data.list[2].main.pressure} mBar`;
-    todayDate2.innerHTML = finalR;
-
-    let temperature3 = Math.floor(data.list[10].main.temp - KELVIN);
-    divTemperature3.innerHTML = `${temperature3} <span>&#8451;</span>`;
-    weatherDesc3.innerHTML = data.list[10].weather[0].description;
-    weatherIcon3.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[10].weather[0].icon}@2x.png">`;
-    divHumidity3.innerHTML = `${data.list[10].main.humidity} %`;
-    divWindSpeed3.innerHTML = `${data.list[10].wind.speed} km/h  ` + result;
-    divPressure3.innerHTML = `${data.list[10].main.pressure} mBar`
-    todayDate3.innerHTML = finalR;
-
-    let temperature4 = Math.floor(data.list[18].main.temp - KELVIN);
-    divTemperature4.innerHTML = `${temperature4} <span>&#8451;</span>`;
-    weatherDesc4.innerHTML = data.list[18].weather[0].description;
-    weatherIcon4.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[18].weather[0].icon}@2x.png">`;
-    divHumidity4.innerHTML = `${data.list[18].main.humidity} %`;
-    divWindSpeed4.innerHTML = `${data.list[18].wind.speed} km/h  ` + result;
-    divPressure4.innerHTML = `${data.list[18].main.pressure} mBar`;
-    todayDate4.innerHTML = finalR;
-
-    let temperature5 = Math.floor(data.list[26].main.temp - KELVIN);
-    divTemperature5.innerHTML = `${temperature5} <span>&#8451;</span>`;
-    weatherDesc5.innerHTML = data.list[26].weather[0].description;
-    weatherIcon5.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[26].weather[0].icon}@2x.png">`;
-    divHumidity5.innerHTML = `${data.list[26].main.humidity} %`;
-    divWindSpeed5.innerHTML = `${data.list[26].wind.speed} km/h  ` + result;
-    divPressure5.innerHTML = `${data.list[26].main.pressure} mBar`;
-    todayDate5.innerHTML = finalR;
-
-    let temperature6 = Math.floor(data.list[34].main.temp - KELVIN);
-    divTemperature6.innerHTML = `${temperature6} <span>&#8451;</span>`;
-    weatherDesc6.innerHTML = data.list[34].weather[0].description;
-    weatherIcon6.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.list[34].weather[0].icon}@2x.png">`;
-    divHumidity6.innerHTML = `${data.list[34].main.humidity} %`;
-    divWindSpeed6.innerHTML = `${data.list[34].wind.speed} km/h  ` + result;
-    divPressure6.innerHTML = `${data.list[34].main.pressure} mBar`;
-    todayDate6.innerHTML = finalR;
-
 }
+
+// getCity("Delhi")
